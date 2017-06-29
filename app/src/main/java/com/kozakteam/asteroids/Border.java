a84d78e1b288d2d93f78a540bcfcbd1fb48522f3
Kamil-Kozak <rydel.poczta@gmail.com>
2017-06-29 17:33:58 +0200

    Add files via upload
    Poprawiony border.

--- app/src/main/java/com/kozakteam/asteroids/Border.java
+++ app/src/main/java/com/kozakteam/asteroids/Border.java
@@ -8,20 +8,27 @@ public class Border extends GameObject {
 
     public Border(float mapWidth, float mapHeight) {
         setType(Type.BORDER);
-        setSize(mapWidth/2, mapHeight/2);
+
+        setLocation(mapHeight/2,mapHeight/2);
+
         float width = mapWidth;
         float height = mapHeight;
-        //każda trójka, to punkt, każde dwie trójki współrzędnych, to jedna linia
+        setSize(width, height);
         float[] borderVertices = new float[]{
-                -width/2, -height/2, 0,
+
+                - width/2, -height/2, 0,
                 width/2, -height/2, 0,
+
                 width/2, -height/2, 0,
                 width/2, height/2, 0,
+
                 width/2, height/2, 0,
                 -width/2, height/2, 0,
+
                 -width/2, height/2, 0,
-                -width/2, -height/2, 0
+                - width/2, -height/2, 0,
         };
+
         setVertices(borderVertices);
     }
 }
