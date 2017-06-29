3023b020642f2fe0841e3b6ce7870cd16a46557d
Kamil-Kozak <rydel.poczta@gmail.com>
2017-06-29 17:44:00 +0200

    Dodanie nowych funkcji.
    Dodana kolizja(statek, pociski,a steroidy).

--- app/src/main/java/com/kozakteam/asteroids/AsteroidsRenderer.java
+++ app/src/main/java/com/kozakteam/asteroids/AsteroidsRenderer.java
@@ -145,6 +145,122 @@ public class AsteroidsRenderer implements Renderer {
                 gameManager.asteroids[i].update(fps);
             }
         }
+
+        // End of all updates!!
+        // All objects are in their new locations
+        // Start collision detection
+        // Check if the ship needs containing
+        if (CollisionDetection.contain(gameManager.mapWidth, gameManager.mapHeight, gameManager.spaceShip.cp)) {
+            lifeLost();
+        }
+
+        // Check if an asteroid needs containing
+        for (int i = 0; i < gameManager.asteroidsNumber; i++) {
+            if (gameManager.asteroids[i].isActive()) {
+                if (CollisionDetection.contain(gameManager.mapWidth, gameManager.mapHeight,
+                        gameManager.asteroids[i].cp)) {
+        // Bounce the asteroid back into the game
+                    gameManager.asteroids[i].bounce();
+        // Play a sound
+                   // sm.playSound("blip");
+                }
+            }
+        }
+
+        // Check if bullet needs containing
+    // But first see if the bullet is out of sight
+    // If it is reset it to make game harder
+        for (int i = 0; i < gameManager.bulletsNumber; i++) {
+    // Is the bullet in flight?
+    // Is the bullet in flight?
+            if (gameManager.bullets[i].isInFlight()) {
+    // Comment the next block to make the game easier!!!
+    // It will allow the bullets to go all the way from
+    // ship to border without being reset.
+    // These lines reset the bullet when
+    // shortly after they leave the players view.
+    // This forces the player to go 'hunting' for the
+    // asteroids instead of spinning round spamming the
+    // fire button...
+    // This code would be better with a viewport.clip() method
+    // like in project 2 but seems a bit excessive just for these
+    // few 15ish lines of code.
+    // Start comment out to make easier
+                pointF = gameManager.bullets[i].getLocation();
+                pointF2 = gameManager.spaceShip.getLocation();
+                if(pointF.x > pointF2.x + gameManager.mToShowX / 2){
+    // Reset the bullet
+                    gameManager.bullets[i].resetBullet(gameManager.spaceShip.getLocation());
+                }else
+                if(pointF.x < pointF2.x - gameManager.mToShowX / 2){
+    // Reset the bullet
+                    gameManager.bullets[i].resetBullet(gameManager.spaceShip.getLocation());
+                }else
+                if(pointF.y > pointF2.y + gameManager.mToShowY/ 2){
+    // Reset the bullet
+                    gameManager.bullets[i].resetBullet(gameManager.spaceShip.getLocation());
+                }else
+                if(pointF.y < pointF2.y - gameManager.mToShowY / 2){
+    // Reset the bullet
+                    gameManager.bullets[i].resetBullet(gameManager.spaceShip.getLocation());
+                }
+    // End comment out to make easier
+    // Does bullet need containing?
+                if (CollisionDetection.contain(gameManager.mapWidth, gameManager.mapHeight,
+                        gameManager.bullets[i].cp)) {
+    // Reset the bullet
+                    gameManager.bullets[i].resetBullet
+                            (gameManager.spaceShip.getLocation());
+    // Play a sound
+                    //sm.playSound("ricochet");
+                }
+            }
+        }
+
+        // Now we see if anything has hit an asteroid
+    // Check collisions between asteroids and bullets
+    // Loop through each bullet and asteroid in turn
+        for (int bulletNum = 0; bulletNum < gameManager.bulletsNumber; bulletNum++) {
+            for (int asteroidNum = 0; asteroidNum < gameManager.asteroidsNumber;
+                 asteroidNum++) {
+    // Check that the current bullet is in flight
+    // and the current asteroid is
+    // active before proceeding
+                if (gameManager.bullets[bulletNum].isInFlight() &&
+                        gameManager.asteroids[asteroidNum].isActive())
+    // Perform the collision checks by
+    // passing in the collision packages
+    // A Bullet only has one vertex.
+    // Our collision detection works on vertex pairs
+                    if (CollisionDetection.detect(gameManager.bullets[bulletNum].cp,
+                            gameManager.asteroids[asteroidNum].cp)) {
+    // If we get a hit...
+                        destroyAsteroid(asteroidNum);
+    // Reset the bullet
+                        gameManager.bullets[bulletNum].resetBullet
+                                (gameManager.spaceShip.getLocation());
+                    }
+            }
+        }
+
+        // Check collisions between asteroids and ship
+    // Loop through each asteroid in turn
+        for (int asteroidNum = 0; asteroidNum < gameManager.asteroidsNumber;
+             asteroidNum++) {
+    // Is the current asteroid active before proceeding
+            if (gameManager.asteroids[asteroidNum].isActive()) {
+    // Perform the collision checks by
+    // passing in the collision packages
+                if (CollisionDetection.detect(gameManager.spaceShip.cp, gameManager.asteroids[asteroidNum].cp)) {
+    // hit!
+                    destroyAsteroid(asteroidNum);
+                    lifeLost();
+                }
+            }
+        }
+
+
+
     }
 
     private void draw() {
@@ -189,6 +305,8 @@ public class AsteroidsRenderer implements Renderer {
         for(int i = 0; i < gameManager.remainingAsteroidsNumber; i++) {
             gameManager.tallyIcons[i].draw();
         }
+
+
     }
 
     public void lifeLost(){
@@ -206,7 +324,7 @@ public class AsteroidsRenderer implements Renderer {
     }
 
     public void destroyAsteroid(int asteroidIndex){
-        gameManager.asteroids[asteroidIndex].setActive(false);
+        gameManager.asteroids[asteroidIndex].setActive(true);
         // zmniejszenie ilosci asteroid
         gameManager.remainingAsteroidsNumber --;
         // czy gracz wyczyscil wszystko?
